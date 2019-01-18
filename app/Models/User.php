d202f56e78f50984fb865ec2efb464454588b3d7
vagrant <vagrant@homestead>
2019-01-18 16:00:58 +0000

    收货地址列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,4 +28,10 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function addresses()
+    {
+        return $this->hasMany(UserAddress::class);
+    }
+    
 }
