0865d24abd87d10061d508ce30a9239d4ef658e3
vagrant <vagrant@homestead>
2019-01-19 12:17:32 +0000

    用户界面-商品详情页

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@
 // Route::get('/', 'PagesController@root')->name('root');
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
-
+Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 // 在之前的路由里加上一个 verify 参数
 Auth::routes(['verify' => true]);
 
